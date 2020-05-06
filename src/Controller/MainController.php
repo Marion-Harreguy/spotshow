b01a7faabdefaca69169a19a9846a00ed63812c9
Marion-Harreguy <marionharreguy@gmail.com>
2020-05-06 14:56:04 +0200

    [func] method add() series

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,7 +5,9 @@ namespace App\Controller;
 use App\Entity\Platform;
 use App\Entity\Serie;
 use App\Repository\SerieRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 
@@ -25,6 +27,28 @@ class MainController extends AbstractController
 
         return $this->render('main/browse.html.twig', [
             'series' => $series,
+        ]);
+    }
+
+    /**
+     * @Route("/series", name="add")
+     */
+    public function add(EntityManagerInterface $em, Request $request)
+    {   
+        $serie = new Serie();
+        $form = $this->createForm(SerieType::class, $serie);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() & $form->isValid()) {
+            $em->persist($serie);
+            $em->flush();
+
+            return $this->redirectToRoute('');
+        }
+
+        return $this->render('main/add.html.twig', [
+            'form' => $form->createView(),
         ]);
     }
 }
