d44c11076096c41340d576aab04422549b5180a6
Marion-Harreguy <marionharreguy@gmail.com>
2020-05-09 11:16:31 +0200

    [func] optimize css with buttons Note

--- src/Entity/Serie.php
+++ src/Entity/Serie.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\SerieRepository")
@@ -17,7 +18,8 @@ class Serie
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=100)
+     * @ORM\Column(type="string", length=100, unique=true)
+     * @Assert\Unique(message="The {{ value}} title already exists.")
      */
     private $title;
 
